08c7c8411cb150708f1885273b350950bd230368
Rafael_Simas <rafaelsimasdev@gmail.com>
2023-08-18 15:32:16 -0300

    [estilo] implementei a estilizacão básica com styled-components

---
+++ viagens-espaciais/src/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style-type: none;
+        overflow-x: hidden;
+    }
+`;
+
+export const FontStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,800;1,400;1,600;1,700&display=swap');
+
+`;
+
+export const Body = styled.div`
+    border: 1px solid red;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    padding: 1% 1%;
+    font-family: 'Poppins', sans-serif;
+    /* background-color: rgb(183, 223, 255); */
+`
---
+++ viagens-espaciais/src/components/Filters/FiltersStyle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const DivFilter = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: start;
+    border: 1px solid red;
+    width: 20%;
+    height: 100%;
+    padding-top: 30px;
+`
+
+export const Form = styled.form`
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center ;
+    gap: 2rem;
+    width: 80%;
+    height: 40%;
+`
